0c6c58ee1d57bcedd183770e1e0a5b780abf64c4
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-02-28 16:56:22 +0530

    Added Recursion

--- index.js
+++ index.js
@@ -1,60 +1,6 @@
-class Graphs {
-  constructor() {
-    this.adjacencyList = {};
-  }
-
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
-      return true;
-    }
-    return false;
-  }
-
-  addEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1].push(vertex2);
-      this.adjacencyList[vertex2].push(vertex1);
-      return true;
-    }
-    return false;
-  }
-
-  removeEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-        (e) => e !== vertex2
-      );
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-        (e) => e !== vertex1
-      );
-      return true;
-    }
-    return false;
-  }
-
-  removeVertex(vertex) {
-    if (!this.adjacencyList[vertex]) return undefined;
-
-    while (this.adjacencyList[vertex].length) {
-      let temp = this.adjacencyList[vertex].pop();
-      this.removeEdge(vertex, temp);
-    }
-    delete this.adjacencyList[vertex];
-    return this;
-  }
+function Recursion(value) {
+  if (value === 1) return 1;
+  return value * Recursion(value - 1);
 }
 
-const myGraph = new Graphs();
-myGraph.addVertex("A");
-myGraph.addVertex("B");
-myGraph.addVertex("C");
-myGraph.addVertex("D");
-myGraph.addEdge("A", "B");
-myGraph.addEdge("A", "C");
-myGraph.addEdge("A", "D");
-myGraph.addEdge("B", "D");
-myGraph.addEdge("C", "D");
-// myGraph.removeEdge("A", "B");
-console.log(myGraph.removeVertex("D"));
-//console.log(myGraph);
+console.log(Recursion(4));
--- readME.md
+++ readME.md
@@ -1,11 +1,3 @@
-# create Graphs and some of their methods
+# create Recursion
 
-- `create Graph of an input`
-- `addVertex method`
-  - to add vertex in a graph
-- `addEdge method`
-  - to add edge in the the graph
-- `removeEdge method`
-  - to remove edge in a graph
-- `removeVertex method`
-  - to remove vertex in a graph
+- `create Recursion of an input`
